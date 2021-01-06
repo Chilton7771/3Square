0f9c52b0363abd163416e1be22b1681fa3dc4ec8
Chilton7771 <70490841+Chilton7771@users.noreply.github.com>
2021-01-07 09:46:36 +1100

    Initial commit

---
+++ Main.py
@@ -0,0 +1,19 @@
+import turtle
+import random
+
+
+window = turtle.Screen()
+window.title('3quare')
+window.bgcolor('white')
+window.setup(width=800, height=600)
+window.tracer(0)
+
+
+# square 1
+square1 = 'unmarked.png'
+screen.addshape(square1)
+turtle.shape(square1)
+square1.goto(-266, 0)
+
+while True:
+    window.update()
